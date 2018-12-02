c8d805c71682415189389324314bf5c5243771b6
SpAwN <31537771+SpAwN2301@users.noreply.github.com>
2018-12-02 11:23:11 +0300

    Add files via upload

---
+++ pr_2/src/com/company/TestAuthor.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Bob Chaplin", "anotheronemail@mail.ru", 'm');
+        System.out.println(a);
+    }
+}
