f8c3007ca6592ebf252c5d902b7761f5f356c158
SpAwN <31537771+SpAwN2301@users.noreply.github.com>
2018-12-02 11:17:01 +0300

    Add files via upload

---
+++ pr_1/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Book b = new Book("Отверженные", "Виктор Гюго", 1800);
+        System.out.println(b);
+    }
+}
